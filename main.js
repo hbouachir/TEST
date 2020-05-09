ece2bea8e776325735b7cf6227c488dc828fc2f8
hbouachir <bouachirhamza@gmail.com>
2020-05-09 16:21:27 +0100

    V0.2

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
 const btn_add=document.querySelector(".add_worker")
 const details=document.querySelectorAll(".worker")
-
+console.log(details);
 let infos;
 Array.from(details).map(el=>{
     el.addEventListener('change',function(e){
-           infos={...infos,[e.target.placeholder]:e.target.value}
+           infos={...infos,
+            [e.target.placeholder]:e.target.value}
+           
+
+
+
             }
             )  }
     
     )
 
-
-
+ 
 
 btn_add.addEventListener('click',function(){
-    let allInfos=JSON.parse(localStorage.getItem('workers'))
-    
-    allInfos=[...allInfos,infos]
-
-    localStorage.setItem('workers', JSON.stringify(Infos));
-
-
-
+   
+    let  allInfos=Array.from(JSON.parse(localStorage.getItem('workers')));
+    allInfos=[...allInfos,infos];
+  
+localStorage.setItem('workers', JSON.stringify(allInfos))
 
 })
 
--- worker.js
+++ worker.js
@@ -1,8 +1,11 @@
+
+
 const liste=document.querySelector('#work_list');
 
 JSON.parse(localStorage.getItem('workers')).map(el=>{
-let structure= `<div><h1>${el.Name}</h1><h3>${el.age}</h3><h2>${el.email}</h2><p>${el.biography}</p></div>`
-let container=ducument.createElement('div')
+let structure= `<div><h1>name   : ${el.Name}</h1><h3>age    :${el.age}</h3><h2>email :${el.email}</h2><p>biography : ${el.biography}</p></div>`
+let container=document.createElement('div')
+
 container.innerHTML=structure
 liste.append(container)
 
