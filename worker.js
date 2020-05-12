8e311d4c8c33f975aba5f23a25cd876063cbc272
hbouachir <bouachirhamza@gmail.com>
2020-05-12 12:45:56 +0100

    V0.5

--- worker.js
+++ worker.js
@@ -15,7 +15,7 @@ let structure=`<div class="card1">
 <a href="#"><i class="fa fa-twitter"></i></a>
 <a href="#"><i class="fa fa-linkedin"></i></a>
 <a href="#"><i class="fa fa-facebook"></i></a>
-<button>X</button>
+<button class="btn-delete">X</button>
 </div>`
 
 
@@ -25,3 +25,20 @@ container.innerHTML=structure
 liste.append(container)
 
 })
+
+const buttons=document.querySelectorAll('button');
+const xbtn=Array.from(buttons).filter(el=>el.className==='btn-delete');
+
+
+xbtn.map(el=>el.addEventListener('click',function(){
+    let y=Array.from(JSON.parse(localStorage.getItem('workers')));
+    let z=el.parentElement.children[0].innerText;
+    let a=y.filter(word=>word.Name!==z);
+   
+     localStorage.removeItem("workers");
+    el.parentElement.remove();
+    localStorage.setItem('workers', JSON.stringify(a))
+
+
+
+}))
